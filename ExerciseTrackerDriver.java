d6be98ab60acd8405d2725c288cb1a4b75efb0a6
hannah27500 <143725521+hannah27500@users.noreply.github.com>
2024-11-22 11:36:33 -0600

    Update ExerciseTrackerDriver.java

--- ExerciseTrackerDriver.java
+++ ExerciseTrackerDriver.java
@@ -1,26 +1,20 @@
-import model.ExerciseTracker;
+/** This is the driver that displays the gui
+ * @author Hannah Evans
+ * @version 1.0
+ * @since 1.0
+*/
+import javax.swing.JFrame;
+
+import gui.ExerciseTrackerGUI;
 
 public class ExerciseTrackerDriver {
-
 	public static void main(String[] args) {
-		
-	ExerciseTracker tracker = new ExerciseTracker();
-		System.out.println(tracker.getExercises());
-		
-		
-		tracker.logAndCalcExercise("Running", 8);
-		System.out.println("RUNNING EXERCISE");
-		System.out.println(tracker.printAllExercises());
-
-		System.out.println("RUNNING & WALKING EXERCISE");
-		tracker.logAndCalcExercise("Walking", 24);
-
-		System.out.println(tracker.printAllExercises());
-		tracker.deleteExercise();
-
-		System.out.println("DELETE RUNNING EXERCISE");
-		System.out.println(tracker.printAllExercises());
+		// Run the GUI and display it
+		ExerciseTrackerGUI frame = new ExerciseTrackerGUI();
+		frame.setTitle("Exercise Tracker");
+		frame.setSize(480, 400);
+		frame.setResizable(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 	}
-
 }
