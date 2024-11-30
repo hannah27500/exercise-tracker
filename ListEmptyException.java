2163cfa3875b3e81de3d55b4953500abd464301c
hannah27500 <143725521+hannah27500@users.noreply.github.com>
2024-11-30 09:45:32 -0600

    Update ListEmptyException.java

--- ListEmptyException.java
+++ ListEmptyException.java
@@ -1,3 +1,8 @@
+/** Exception for when linked list is empty
+ * @author Hannah Evans
+ * @version 1.0
+ * @since 1.0
+*/
 package model;
 
 public class ListEmptyException extends RuntimeException {
